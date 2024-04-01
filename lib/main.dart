ce0dc9b88b7ab4382a97722b9a05a0ef0e7f07d2
Yaquelin Burgos Saavedra <Yaquelin.Burgos@UPDS.edu.bo>
2024-04-01 17:59:11 -0400

    01-04-2024 17:59

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:peliculas/screens/screens.dart';
 
 
-void main() => runApp( MyApp());
+void main() {
+  runApp( MyApp());
+}
 
 class MyApp extends StatelessWidget {
   
