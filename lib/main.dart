bb9d2503e879416aafeeca849272ebeef832988c
Yaquelin Burgos Saavedra <Yaquelin.Burgos@UPDS.edu.bo>
2024-04-05 17:28:11 -0400

    05/04/2024 17:28

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:peliculas/screens/screens.dart';
 
-
 void main() {
-  runApp( MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:peliculas/widgets/widgets.dart';
 
+
+
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
