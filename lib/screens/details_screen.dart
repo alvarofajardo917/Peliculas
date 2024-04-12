54830be05cf84bf0e01a3b00b9b2723c812fcd59
Yaquelin Burgos Saavedra <Yaquelin.Burgos@UPDS.edu.bo>
2024-04-12 18:59:00 -0400

    12/04/2024 18:58

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class DetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //TODO:Cambiar luego por una instancia movie
-    final String movie =ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+    final TextTheme textTheme = Theme.of(context).textTheme;
         
-
     return Scaffold(
         body: CustomScrollView(
-      slivers: [_CustomAppBar()],
+      slivers: [
+        _CustomAppBar(),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              _PosterAndTitle()
+            ])
+        )
+        ],
     ));
   }
 }
@@ -39,6 +46,49 @@ class _CustomAppBar extends StatelessWidget {
           image:NetworkImage('https://via.placeholder.com/500x300'),
           fit: BoxFit.cover,
           ),
+      ),
+    );
+  }
+}
+
+
+
+class _PosterAndTitle extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'),
+              image:NetworkImage('https://via.placeholder.com/200x300') ,
+              height: 150,
+            ),
+          ),
+          
+          SizedBox(width: 20),
+
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('movie.title',style: Theme.of(context).textTheme.headlineSmall,overflow: TextOverflow.ellipsis,maxLines: 2),
+              Text('movie.originalTitle',style: Theme.of(context).textTheme.headlineSmall,overflow: TextOverflow.ellipsis),
+
+            Row(
+              children: [
+                Icon(Icons.star_outline,size: 15,color: Colors.grey),
+                SizedBox(width: 5),
+                Text('movie.voteAverage',style: Theme.of(context).textTheme.bodySmall)
+              ],
+            )
+            ],
+          )
+        ],
       ),
     );
   }
